3926c411e9869a3c31fe2236340c6d498cdd3b06
Hassan Azimi <hassanazimi@users.noreply.github.com>
2021-12-11 17:34:16 +0000

    7

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -15,12 +15,33 @@ const article = ({ article }) => {
 	)
 }
 
-export const getServerSideProps = async context => {
+// export const getServerSideProps = async context => {
+// 	const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
+// 	const article = await response.json();
+// 	return {
+// 		props: { article }
+// 	}
+// }
+
+export const getStaticProps = async context => {
 	const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
 	const article = await response.json();
 
 	return {
 		props: { article }
+	}
+}
+
+export const getStaticPaths = async () => {
+	const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+
+	const articles = await response.json();
+	const ids = articles.map(article => article.id);
+	const paths = ids.map(id => ({ params: { id: id.toString() } }));
+
+	return {
+		paths,
+		fallback: false
 	}
 }
 
